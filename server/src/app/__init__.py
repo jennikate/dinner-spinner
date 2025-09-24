7994b037e041d466f232a8b1d51cfb7e40b2314e
Jennifer Wallace <jennikate@gmail.com>
2025-09-24 11:36:03 +0100

    Setup app
    Setup gitignore
    
    Setup folder structure and instructions
    
    Update requirements
    
    Add logging
    
    Setup extensions
    
    Add pytest config
    
    Update gitignore
    
    Remove pyc files
    
    Remove pyc files

---
+++ server/src/app/__init__.py
@@ -0,0 +1,43 @@
+"""
+Providing the create_app() function that applies config and 
+returns the Flask application instance.
+"""
+###################################################################################################
+# Imports
+###################################################################################################
+
+import logging
+import os
+
+from flask import Flask
+
+from .config import config
+from .extensions import api, db
+
+###################################################################################################
+# Body
+###################################################################################################
+def register_blueprints(app):
+    app.logger.debug("---------- Starting register_blueprints ----------")
+    app.logger.debug("---------- Finished register_blueprints ----------")
+
+
+def register_extensions(app):
+    app.logger.debug("---------- Starting register_extensions ----------")
+    db.init_app(app)
+    api.init_app(app)
+    app.logger.debug("---------- Finished register_extensions ----------")
+
+
+def create_app(config_name):
+    """
+    Create a Flask application using the app factory pattern. Registers extensions and blueprints using
+    local functions.
+    """
+    app = Flask(__name__)
+    app.logger.info("---------- Starting create_app ----------")
+    app.config.from_object(config[config_name])
+    register_extensions(app)
+    register_blueprints(app)
+    app.logger.info("---------- Finished create_app ----------")
+    return app
