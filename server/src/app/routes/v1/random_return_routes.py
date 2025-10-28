dbd038ac9964727f387ab770088b48d9ca34b900
Jennifer Wallace <jennikate@gmail.com>
2025-10-28 13:43:06 +0000

    Return random recipes with pinned

--- server/src/app/routes/v1/random_return_routes.py
+++ server/src/app/routes/v1/random_return_routes.py
@@ -15,10 +15,14 @@ QueryParams
 
 from flask import current_app
 from flask.views import MethodView
-from flask_smorest import Blueprint
+from flask_smorest import Blueprint, abort
+from sqlalchemy import func
 
 from ...constants import DEFAULT_RANDOM_RECIPES
-from ...schemas.recipes import RandomRecipeListSchema
+from ...extensions import db
+from ...models import Recipe
+from ...schemas.generic import ErrorSchema
+from ...schemas.recipes import RandomRecipeListRequestSchema, RandomRecipeListSchema
 
 
 # =====================================
@@ -29,61 +33,54 @@ blp = Blueprint("random", __name__, url_prefix="/v1", description="Querying for 
 
 @blp.route("/random")
 class RandomRecipeListResource(MethodView):
+    @blp.arguments(RandomRecipeListRequestSchema())
     @blp.response(200, RandomRecipeListSchema(many=True))
-    def get(self, args):
+    @blp.alt_response(400, schema=ErrorSchema(), description="Bad request - invalid input")
+    def post(self, args):
         """
-        Get list of random recipes
+        Posts a request for a list of random recipes
+        Within the POST the request can contain a 
+        -> number: the number of recipes to return
+        -> pin: a list of uuids to keep and return as part of the response
+        
         Returns list of ID and NAME, unpaginated
         """
         current_app.logger.debug("---------- Starting Get Random Recipes ----------")
         current_app.logger.debug(f"Getting recipes with args: {args}")
 
-        current_app.logger.debug("---------- Finished Get Random Recipes ----------")
+        # CHECK UUIDs AND CREATE LIST OF RECIPES TO RETURN BASED ON ARGS
+        current_app.logger.debug("---> Handle pin recipe requests")
+        current_app.logger.debug(f"Ids to pin: {args.get("pin")}")
+
+        pins = args.get("pin", [])
+        recipes_to_keep = []
+        invalid_pins = []
+
+        for id in pins:
+            current_app.logger.debug(f"Looking for recipe: {id}")
+            recipe = db.session.get(Recipe, id)
+            if not recipe:
+                invalid_pins.append(id)
+            else:
+                recipes_to_keep.append(recipe)
+
+        if invalid_pins != []:
+            abort(404, message=f"You have invalid recipe IDs: {invalid_pins}")
         
-# return DEFAULT_RANDOM_RECIPES amount of random recipe IDs and NAME
-# if number= in queryparam then returne NUMBER amount of random recipes IDs and NAME
-# if pin= in queryparam then 
-# -> check ID is a valid UUID
-# -> check ID exists as a recipe.id
-# -> store that ID to exclude_list
-# -> create number_to_return = number/DEFAULT_RANDOM_RECIPES - len(exclude_list)
-# -> return exclude_list + random_list
+        # CALCULATE NUMBER TO RANDOMLY GENERATE
+        current_app.logger.debug("---> Calculate number recipes to get")
+        current_app.logger.debug(f"Pinned recipes: {recipes_to_keep}")
+        number_to_return = (args.get("number") or DEFAULT_RANDOM_RECIPES) - len(recipes_to_keep)
+        
+        current_app.logger.debug(f"Requested: {args.get("number")}, Pinned: {len(recipes_to_keep)}, To randomly select: {number_to_return}")
 
-# recipe_schema = RecipeSchema(many=True)  # many=True because we return a list
+        # GET THE RANDOM RECIPES
+        current_app.logger.debug("---> Get random recipes")
+        random_recipes = Recipe.query.order_by(func.random()).limit(number_to_return).all()
+        current_app.logger.debug(f"Randomly found receipes: {random_recipes}")
+        
+        result = recipes_to_keep + random_recipes
+        current_app.logger.debug(f"Return list: {result}")
 
-# @blp.route("/random", methods=["GET"])
-# def get_random_recipes():
-#     # Query 7 random Recipe objects
-#     random_recipes = Recipe.query.order_by(func.random()).limit(7).all()
-#     # Serialize them using Marshmallow
-#     return recipe_schema.dump(random_recipes)
-
-
-# @blp.route("/random", methods=["GET"])
-# def get_random_recipes():
-#     excluded_ids = [1, 5, 9]  # IDs you want to exclude
-
-#     # Query 7 random Recipe objects excluding the given IDs
-#     random_recipes = (
-#         Recipe.query
-            # .filter(Recipe.id.notin_(excluded_ids))
-#         .order_by(func.random())
-#         .limit(7)
-#         .all()
-#     )
-
-#     # Serialize with Marshmallow
-#     return recipe_schema.dump(random_recipes)
-
-
-
-# # Get excluded IDs from query parameters, e.g., /recipes/random?exclude=1,5,9
-#     exclude_param = request.args.get("exclude", "")
-
-
-
-# # Convert string like "1,5,9" into a list of integers
-#     if exclude_param:
-#         excluded_ids = [int(x) for x in exclude_param.split(",") if x.isdigit()]
-#     else:
-#         excluded_ids = []
+        current_app.logger.debug("---------- Finished Get Random Recipes ----------")
+        return result
